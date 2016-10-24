941fb8e3974a6f5947c72d24e128a35d24d72aba
snowy6409 <snowy6409@comcast.net>
2016-10-24 17:54:46 -0400

    Added A Scripts Folder

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    public string[] Items;
+    public string[] Potions;
+    public int Currency;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I))
+        { 
+        
+            foreach (var item in Items)
+            {
+               Debug.Log(item.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (var item in Potions)
+            {
+                Debug.Log(item.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+                Debug.Log(Currency + " Credits" );
+            
+        }
+
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if(string.IsNullOrEmpty(Items[19]))
+        {
+            for (int i = 0; i <= 19; i++)
+            {
+                Debug.Log("1");
+                if(string.IsNullOrEmpty(Items[i]) && Items[i] != collision.gameObject.name)
+                {
+                    Debug.Log("2");
+                    Items[i] = collision.gameObject.name;
+                    return;
+                }
+                else
+                {
+                    Debug.Log("3");
+                    i += 1;
+                }
+
+
+
+
+            }
+
+        }
+        else
+        {
+
+            Debug.Log("Inventory full");
+            Debug.Log("Can't collect " + collision.gameObject.name);
+            Debug.Log(Items[19]);
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
