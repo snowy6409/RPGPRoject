f4704511ae0961834c31edc13c09ab858b91af56
snowy6409 <snowy6409@comcast.net>
2016-10-23 21:59:59 -0400

    Added Basic Inventory System
    This is the most basic of Inventory Systems.

---
+++ Inventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    public string[] Items;
+    public string[] Potions;
+    public int Currency;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I))
+        { 
+        
+            foreach (var item in Items)
+            {
+               Debug.Log(item.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (var item in Potions)
+            {
+                Debug.Log(item.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+                Debug.Log(Currency + " Credits" );
+            
+        }
+
+    }
+}
